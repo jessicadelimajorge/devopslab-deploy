2ec9bdea86b999469dee3c8424817917688b8e46
jessicadelimajorge <77299370+jessicadelimajorge@users.noreply.github.com>
2023-04-23 21:23:16 -0300

    VGravacao

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps - vFinal"
+    return "Laboratório Pipeline DevOps - vGravacao"
 
 if __name__ == '__main__':
     app.run()
